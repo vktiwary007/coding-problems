cf2c214af4be88e4c1e92bee5ae1763f06cc1b60
vktiwary007 <vktiwary@asu.edu>
2020-01-11 18:00:35 -0700

    leetcode concurrency

---
+++ src/com/vivekasu/PrintInOrder.java
@@ -0,0 +1,41 @@
+package com.vivekasu;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PrintInOrder {
+
+    private AtomicInteger firstjob = new AtomicInteger(0);
+    private AtomicInteger secondjob = new AtomicInteger(0);
+
+    public PrintInOrder() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+
+        firstjob.incrementAndGet();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        while(firstjob.get() !=1){
+
+        }
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+
+        secondjob.incrementAndGet();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        while(secondjob.get() !=1){
+
+        }
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
