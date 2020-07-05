ee0b4c6123115e4cdadfd60d9ae651d2d586eda9
vktiwary007 <vktiwary@asu.edu>
2020-07-04 20:39:40 -0700

    solution for leetcode problem Intersection of two LinkedList

---
+++ src/com/vivekasu/IntersectionTwoLL.java
@@ -0,0 +1,22 @@
+package com.vivekasu;
+
+public class IntersectionTwoLL {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        if(headA==null || headB==null)
+            return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while(a!=b){
+
+            a = a==null?headB:a.next;
+            b = b==null?headA:b.next;
+
+        }
+
+        return a;
+    }
+}
