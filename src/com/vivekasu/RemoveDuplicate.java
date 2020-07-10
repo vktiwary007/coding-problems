0a077d21bd1ffb8c83057d2abc50546af63539d0
vktiwary007 <vktiwary@asu.edu>
2020-07-09 22:00:47 -0700

    solution for leetcode problem Remove duplicates from sorted array in-place

--- src/com/vivekasu/RemoveDuplicate.java
+++ src/com/vivekasu/RemoveDuplicate.java
@@ -2,5 +2,27 @@ package com.vivekasu;
 
 public class RemoveDuplicate {
 
+    public int removeDuplicates(int[] nums) {
+
+        int n = nums.length;
+        if(n<1)
+            return 0;
+
+        int len = 0;
+        nums[len++] = nums[0];
+        for(int i=1; i<nums.length; i++){
+
+            int prev = i;
+            while(i<nums.length && nums[i]==nums[i-1]) i++;
+            if(i<nums.length)
+                nums[len++] = nums[i];
+
+        }
+
+
+        return len;
+
+    }
+
 
 }
