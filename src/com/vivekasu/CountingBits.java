8260e150bc6db93b72a4f551e70ca723e43e6115
vktiwary007 <vktiwary@asu.edu>
2020-07-15 14:53:00 -0700

    solution for leetcode problem Counting Bits

---
+++ src/com/vivekasu/CountingBits.java
@@ -0,0 +1,18 @@
+package com.vivekasu;
+
+public class CountingBits {
+
+    public int[] countBits(int num) {
+
+        int[] ans = new int[num+1];
+        ans[0] = 0;
+
+        for(int i=1;i<=num;i++){
+
+            ans[i] = ans[i&(i-1)] + 1;
+
+        }
+
+        return ans;
+    }
+}
